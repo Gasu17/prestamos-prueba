943f2e5a4942c342ae0feb009985cd49aada3796
Gasu17 <sbrofati.alessandro@gmail.com>
2026-02-17 10:48:06 +0100

    Añadiendo los implemetns en el repository

--- src/main/java/es/fplumara/dam1/prestamos/repository/BaseRepository.java
+++ src/main/java/es/fplumara/dam1/prestamos/repository/BaseRepository.java
@@ -1,33 +1,41 @@
 package es.fplumara.dam1.prestamos.repository;
 
 
+import es.fplumara.dam1.prestamos.csv.CSVMaterialImporter;
+import es.fplumara.dam1.prestamos.csv.RegistroMaterialCsv;
 import es.fplumara.dam1.prestamos.model.Identificable;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.nio.file.Path;
+import java.util.*;
+
 
 public class BaseRepository<T extends Identificable> implements Repository<T> {
-    Map<String, T> datos;
+
+
+    Map<String, T> datos = new HashMap<>();
+
 
     @Override
     public void save(T e) {
+        datos.put(e.getId(), e);
 
     }
 
     @Override
     public Optional<T> findById(String id) {
-        return Optional.empty();
+
+
+        return Optional.ofNullable(datos.get(id));
     }
 
     @Override
     public List<T> listAll() {
-        return List.of();
+        return new ArrayList<>(datos.values());
     }
 
     @Override
     public void delete(String id) {
+        datos.remove(id);
 
     }
 }
