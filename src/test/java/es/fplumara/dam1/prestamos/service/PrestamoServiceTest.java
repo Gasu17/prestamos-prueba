755ed84a8590fb3ac73adc409a46468ae2971f94
Gasu17 <sbrofati.alessandro@gmail.com>
2026-02-20 13:50:03 +0100

    para debugear

--- src/test/java/es/fplumara/dam1/prestamos/service/PrestamoServiceTest.java
+++ src/test/java/es/fplumara/dam1/prestamos/service/PrestamoServiceTest.java
@@ -1,6 +1,8 @@
 package es.fplumara.dam1.prestamos.service;
 
 
+import es.fplumara.dam1.prestamos.exception.MaterialNoDisponibleException;
+import es.fplumara.dam1.prestamos.exception.NoEncontradoException;
 import es.fplumara.dam1.prestamos.model.EstadoMaterial;
 import es.fplumara.dam1.prestamos.model.Material;
 import es.fplumara.dam1.prestamos.model.Prestamo;
@@ -23,6 +25,7 @@ import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -47,9 +50,9 @@ class PrestamosServiceTest {
     void setup() {
         proyector = new Proyector(
                 "123",
-                "acerp",
+                "ejemploPoryectir",
                 EstadoMaterial.DISPONIBLE,
-                Set.of("eitqueta" , "asd"),
+                Set.of("eitqueta", "asd"),
                 23
 
         );
@@ -73,12 +76,46 @@ class PrestamosServiceTest {
         );
         //ASSERT
         assertEquals(EstadoMaterial.PRESTADO, proyector.getEstadoMaterial());
-        verify(prestamoRepository).save(any());
+        // verify(prestamoRepository).save(any());
     }
 
     // - crearPrestamo_materialNoExiste_lanzaNoEncontrado()
+    @Test
+    void crearPrestamo_materialNoExiste_lanzaNoEncontrado() {
+
+
+        when(materialRepository.findById(anyString())).thenReturn(Optional.empty());
+
+        assertThrows(NoEncontradoException.class, () -> {
+            prestamoService.crearPrestamo("123", "ivan", LocalDate.now());
+        });
+    }
+
+
     // - crearPrestamo_materialNoDisponible_lanzaMaterialNoDisponible()
+    @Test
+    void crearPrestamo_materialNoDisponible_lanzaMaterialNoDisponible() {
+
+        when(materialRepository.findById(anyString()))
+                .thenReturn(Optional.of(proyector));
+
+        assertThrows(MaterialNoDisponibleException.class, () -> {
+            prestamoService.crearPrestamo(proyector.getId(), "IVAN", LocalDate.now());
+        });
+    }
+
+
     // - devolverMaterial_ok_cambiaADisponible()
-    //
+    @Test
+    void devolverMaterial_ok_cambiaADisponible() {
+        when(materialRepository.findById(anyString()))
+                .thenReturn(Optional.of(proyector));
+
+        EstadoMaterial estado = EstadoMaterial.PRESTADO;
+        Prestamo prestamo = prestamoService.crearPrestamo(proyector.getId(), "Ivan0", LocalDate.now());
+        assertEquals(estado, () -> {
+            );
+        } );
+    }
     // Requisito: usar mocks de repositorios y verify(...)
 }
